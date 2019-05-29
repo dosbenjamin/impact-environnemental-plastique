136688680fa915490997bac7ec4d26d6ab809910
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-05-29 21:19:46 +0200

    add data animation beta

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
-console.info('Hello world');
+const datas = document.querySelectorAll('.data')
+
+datas.forEach(data => {
+  data.addEventListener('click', e => {
+    const stat = e.target.getAttribute('data-stats')
+    const toDisplay = document.querySelector(
+      `.infos__item[data-info="${stat}"]`
+    )
+    toDisplay.style.display = 'block'
+  })
+})

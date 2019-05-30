7cc24ed00dfc97e4591741275f701af1f4a7bdce
Benjamin Dos Santos <dosbenjamin@gmail.com>
2019-05-30 16:17:18 +0200

    add production datas

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,23 @@
 const datas = document.querySelectorAll('.data')
+const dataReference = document.querySelector('.data[data-stats="2050"]')
 
 datas.forEach(data => {
   data.addEventListener('click', e => {
     const stat = e.target.getAttribute('data-stats')
+
+    const active = document.querySelector('.infos__item--active')
+    active != null && active.classList.remove('infos__item--active')
+
     const toDisplay = document.querySelector(
       `.infos__item[data-info="${stat}"]`
     )
-    toDisplay.style.display = 'block'
+    toDisplay.classList.add('infos__item--active')
+  })
+})
+
+const infoButton = document.querySelectorAll('.infos__button')
+infoButton.forEach(button => {
+  button.addEventListener('click', e => {
+    e.target.parentElement.classList.remove('infos__item--active')
   })
 })
